651c665ef6c01f0e23c9d2d8b4b74486238a19e9
Murilo Prianti <murilo.silva@sou.inteli.edu.br>
2024-12-05 15:58:46 -0300

    ta funcionando porra

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # SLAM_Aplication
+
+# Como se conectar com o robô.
+
+Conecte o robô com o ssh:
+
+export ROS_DOMAIN_ID=<NUMERO_ABAIXO_DE_214>
+
+Fazer no computador tambem então tem que abrir outro terminal e fazer isso.
+
+Configurando o modelo do robô como burger
+
+export TURTLEBOT3_MODEL=burger
+
+Não esqueça de rodar o bringup no robô:
+
+ros2 launch turtlebot3_bringup robot.launch.py
---
+++ workspace/src/slam/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'slam'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='inteli',
+    maintainer_email='murilo.silva@sou.inteli.edu.br',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
